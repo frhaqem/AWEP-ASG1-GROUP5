695a50d6a8a5014efcefdcd580831950733c0c2f
Zam <82580772+Zammu-max@users.noreply.github.com>
2022-02-25 11:37:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,39 +4,50 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login page</title>
-
-
   <style type="text/css">
     html{
-      background-color: #33cccc;
+      background-color: #66b3ff;
     }
+
     #loginprompt{
       text-align: center;
-      margin-top: 35%;
+      margin-top: 50px;
       border: 1px solid black;
-      background-color: #ffff99;
+      background-color: #ff3333;
       height: 200px;
       width: 100%;
     }
-  </style>
 
+    img{
+      width: 400px;
+      height: 300px;
+      }
+
+      #pb{
+        text-align: center;
+        margin-top: 10%;
+      }
+
+  </style>
 </head>
 <body>
 
+  <div id="pb">
+    <img src="pblogo.png">
+  </div>
   
-
-  <div id="loginprompt" >
+  <div id="loginprompt">
     <h1>Login to access page</h1>
-    <form action="loginaction.php" name="loginpage" method="post">
+    <form action="homepage.php" name="loginpage" method="post">
       <p>Username:<input type="text" name="user" id="user" placeholder="Enter Username"></p>
-
       <p>Password: <input type="password" name="pass" id="pass" placeholder="Enter Password"></p>
-
       <input type="submit" name="submit" value="Login">
       
-
     </form>
   </div>
 
+  <?php 
+
+   ?>
 </body>
 </html>
